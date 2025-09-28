5b254d3ce9937bb516e6ad90ca143543548c964f
kjbddo <kjbddo@naver.com>
2025-09-28 19:52:00 +0900

    Initial commit: Spring Boot BitComputer project setup

---
+++ src/main/java/com/example/bitcomputer/BitComputerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitcomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitComputerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitComputerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bitcomputer/BitComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitcomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitComputerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

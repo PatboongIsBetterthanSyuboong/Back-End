40abab9b1d2d802ec1cc11c0718eb89572f2cd21
JunLennon <jun030516@gmail.com>
2025-11-11 11:46:22 +0900

    fix: add test DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,23 @@
 INSERT INTO disease (id, code, name)
-VALUES (1, 'A', 'Cholera')
+VALUES (1, 'A001', 'Cholera')
+ON DUPLICATE KEY UPDATE code = VALUES(code), name = VALUES(name);
+
+INSERT INTO disease (id, code, name)
+VALUES (2, 'A002', 'HIV')
 ON DUPLICATE KEY UPDATE code = VALUES(code), name = VALUES(name);
 
 INSERT INTO diagnose (id, code, name, dose, time, days)
-VALUES (1, 'A', 'Ducoral sol', 3, UNIX_TIMESTAMP(), 3)
+VALUES (1, 'B001', 'Ducoral sol', 3, UNIX_TIMESTAMP(), 3)
 ON DUPLICATE KEY UPDATE code = VALUES(code),
                         name = VALUES(name),
                         dose = VALUES(dose),
                         time = VALUES(time),
                         days = VALUES(days);
 
+INSERT INTO diagnose (id, code, name, dose, time, days)
+VALUES (2, 'B002', 'Shiver Stop', 5, UNIX_TIMESTAMP(), 2)
+ON DUPLICATE KEY UPDATE code = VALUES(code),
+                        name = VALUES(name),
+                        dose = VALUES(dose),
+                        time = VALUES(time),
+                        days = VALUES(days);
